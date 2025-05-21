f4d8806dd90878c18ba7a8383915dfaca5dc8592
Lovro <lovro.posaric@gmail.com>
2025-05-21 08:02:14 +0200

    Modify Task entity with an appropriate Date converter

--- app/src/main/java/hr/foi/rmai/memento/entities/Task.kt
+++ app/src/main/java/hr/foi/rmai/memento/entities/Task.kt
@@ -1,10 +1,32 @@
 package hr.foi.rmai.memento.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import hr.foi.rmai.memento.helpers.DateConverter
 import java.util.Date
 
-data class Task(
-    val name: String,
-    val dueDate: Date,
-    val course: TaskCourse,
-    val completed: Boolean
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [ForeignKey(
+        entity = TaskCourse::class,
+        parentColumns = ["id"],
+        childColumns = ["course_id"],
+        onDelete = ForeignKey.RESTRICT
+    )]
 )
+@TypeConverters(DateConverter::class)
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    @ColumnInfo(name = "due_date") val dueDate: Date,
+    @ColumnInfo(name = "course_id", index = true) val courseId: Int,
+    val completed: Boolean
+) {
+    @Ignore
+    lateinit var course: TaskCourse
+}
