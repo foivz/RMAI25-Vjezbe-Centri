c879e6dabfb5b1f49e33bf10d1b8f474cbdf1ea8
Lovro <lovro.posaric@gmail.com>
2025-05-21 08:01:40 +0200

    Modify TaskCourse entity to become a database entity

--- app/src/main/java/hr/foi/rmai/memento/entities/TaskCourse.kt
+++ app/src/main/java/hr/foi/rmai/memento/entities/TaskCourse.kt
@@ -1,6 +1,14 @@
 package hr.foi.rmai.memento.entities
 
-data class TaskCourse(val name: String, val color: String) {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_courses")
+data class TaskCourse(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val color: String
+) {
     override fun toString(): String {
         return name
     }
