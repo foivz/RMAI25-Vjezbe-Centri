bd43a75b8373f159682db7089975bb9032f4bd00
Lovro <lovro.posaric@gmail.com>
2025-05-21 08:00:08 +0200

    Add KSP and Room dependencies for local database usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -36,6 +38,11 @@ android {
 }
 
 dependencies {
+    val room_version = "2.7.1"
+
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
